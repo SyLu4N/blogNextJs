763ba90685bf017bcbefe83a8aa6f5e218bc3260
Luan Simões <luaan.carlos@hotmail.com>
2022-06-12 02:21:21 -0300

    add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,41 @@
+import { BsCreditCard2Front } from 'react-icons/bs';
+import { BiSend } from 'react-icons/bi';
+import { FaLinkedinIn } from 'react-icons/fa';
+
 import styles from './footer.module.scss';
 
 export function Footer(): JSX.Element {
   return (
-    <p className={styles.footer}>
-      Feito com ❤️ por
-      <a
-        href="https://github.com/SyLu4N/blogNextJs"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Luan Simões
-      </a>
-    </p>
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerContent}>
+        <div>
+          <h1>SyLu4N</h1>
+          <p>Frontend Developer</p>
+        </div>
+        <nav>
+          <a href="https://sylu4n.vercel.app/" target="_blank" rel="noreferrer">
+            <BsCreditCard2Front />
+            Portfolio
+          </a>
+          <a
+            href="mailto:luaan.carlos@hotmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BiSend />
+            Contato
+          </a>
+          <a
+            href="https://www.linkedin.com/in/luan-sim%C3%B5es-617492236/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedinIn />
+            LinkedIn
+          </a>
+        </nav>
+      </div>
+      <em>© Luan Simões. All right reserved</em>
+    </footer>
   );
 }
